b99b4337a40e24bf2a124d727600b86d4405a983
gusanitor8 <gusanitor8@gmail.com>
2021-09-12 13:53:31 -0600

    Primeras modificaciones a Vista

--- src/Laboratorio2.java
+++ src/Laboratorio2.java
@@ -1,13 +1,38 @@
 public class Laboratorio2 {
     public static void main(String[] args){
+        init();
+
         boolean condition = true;
         while(condition){
             switch(Vista.menuDeOpciones()){
                 case 1:
                     System.out.println("Ingresaste la opcion 1 \n\n");
+                    break;
                 case 9:
                     condition = false;
-            };
+                    break;
+                default:
+                    System.out.println("Esa opcion no es valida intenta de nuevo");
+                    break;
+            }
+        }
+    }
+
+    public static void init(){
+        boolean condicion = true;
+
+        while(condicion){
+            switch (Vista.choseRAM()){
+                case 1:
+                    System.out.println("eligio " + Vista.choseSDR() + "gb");
+                    condicion = false;
+                    break;
+                case 2:
+                    condicion = false;
+                    break;
+                default:
+                    System.out.println("Por favor elija una  de las opciones");
+            }
         }
     }
 }
--- src/Vista.java
+++ src/Vista.java
@@ -4,7 +4,7 @@ import java.util.InputMismatchException;
 public class Vista {
     private static Scanner input = new Scanner(System.in);
 
-    private static int choseRAM(){
+    public static int choseRAM(){
         System.out.println("Ingrese el tipo de RAM que va a usar: \n"+
                             "1. SDR\n" +
                             "2. DDR");
@@ -15,7 +15,9 @@ public class Vista {
             option = input.nextInt();
         }catch(InputMismatchException e){
             System.out.println("Por favor ingrese un numero entero");
+            input.next();
         }
+        return option;
     }
 
     public static int menuDeOpciones(){
@@ -35,8 +37,36 @@ public class Vista {
             System.out.println("Ingrese una opcion: ");
             option = input.nextInt();
         }catch(InputMismatchException e){
-            System.out.println("Por favor ingrese un numero entero \n");
+            System.out.println("Por favor ingrese un numero   entero \n");
+            input.next();
         }
         return option;
     }
+
+    public static int choseSDR(){
+        System.out.println("\t Elija un tamanio de memoria SDR: \n" +
+                            "\t 1. 4gb\n"+
+                            "\t 2. 8gb\n"+
+                            "\t 3. 12gb\n" +
+                            "\t 4. 16gb\n" +
+                            "\t 5. 32gb\n" +
+                            "\t 6. 64gb");
+
+        int[] capacities = {4,8,12,16,32,64};
+        int capacity = 0;
+
+        try{
+            capacity = capacities[input.nextInt() - 1 ];
+        }catch(InputMismatchException e){
+            System.out.println("Ingrese un numero entero");
+            input.next();
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("El numero que usted ingreso no esta dentro de las opciones");
+            capacity = choseSDR();
+        }
+        //System.out.println("Usted eligio " + capacity + "gb");
+        return capacity;
+    }
+
+
 }
